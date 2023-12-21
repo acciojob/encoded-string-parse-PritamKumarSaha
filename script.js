8e7a2413c09bc654c4f3927546de7241b06b2026
PritamKumarSaha <137249952+PritamKumarSaha@users.noreply.github.com>
2023-12-21 20:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const parseCode = (str) => {
   // your code here
+	let splitingStr = str.split(0);
+	let ans=[];
+	
+	for(let i=0; i<splitingStr.length; i++) {
+		if(splitingStr[i] === undefined) {
+			continue;
+		}
+		else {
+			ans.push(splitingStr[i]);
+		}
+	}
+	let [a,b,c] = ans;
+	return{
+		firstName: a,
+		lastName: b,
+		id: c
+	}
 };
 
 // Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+// const str = prompt("Enter str: ");
+// alert(JSON.stringify(parseCode(str)));
